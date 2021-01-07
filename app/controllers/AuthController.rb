5250723a1e51db6cc77580436887ad7f23a10079
Janu <janu.m.sung@gmail.com>
2021-01-06 20:46:44 -0800

    predeployment commit

--- app/controllers/AuthController.rb
+++ app/controllers/AuthController.rb
@@ -2,14 +2,10 @@ class AuthController < ApplicationController
   # /login
     def create
       #.includes(:items) will serialize the items associated to user, switch with folders/workouts once they are built? or just add them to serializer
-
-      # user = User.includes(:items).find_by(username: user_params[:username]) 
       @user = User.find_by(username: user_params[:username])
 
       if @user && @user.authenticate(user_params[:password])
         jwt = self.issue_token(@user)
-        
-        # render json: {user: user, jwt: jwt}, include: [ :items ]
         render json: {user: @user, jwt: jwt}
 
       else
@@ -20,7 +16,6 @@ class AuthController < ApplicationController
     def show
       @user = User.find(self.user_id)
       if @user && self.logged_in?
-        # render json: {user: user, jwt: self.token}, include: [:items]
         render json: {user: @user, jwt: self.token}
       else
         render json: { error: 'User cannot be found', status: 401 }
