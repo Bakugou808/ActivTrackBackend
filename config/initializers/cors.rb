a647e26a1e87f445e1074b54aa423969dea3b811
Janu <janu.m.sung@gmail.com>
2021-01-08 15:56:25 -0800

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://www.activtrack.xyz'
+    origins 'https://activtrack.herokuapp.com/'
 
     resource '*',
       headers: :any,
