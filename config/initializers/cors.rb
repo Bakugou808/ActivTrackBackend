20e26c6ced9e34ad39bbf8593d31234d8db7a729
Janu <janu.m.sung@gmail.com>
2021-01-14 20:19:31 -0800

    routine commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://activtrack.herokuapp.com'
+    origins '*'
+    # origins 'https://activtrack.herokuapp.com'
 
     resource '*',
       headers: :any,
